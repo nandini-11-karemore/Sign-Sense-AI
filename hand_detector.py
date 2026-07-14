119a190c5a03fc96d9447141685275ed18acacca
nanthedino <nandinikaremore4@gmail.com>
2026-07-14 12:36:52 +0530

    Check MediaPipe package

--- hand_detector.py
+++ hand_detector.py
@@ -1,32 +1,6 @@
-import cv2
 import mediapipe as mp
+import streamlit as st
 
-
-class DetectionResult:
-    def __init__(self, hand_landmarks):
-        self.hand_landmarks = hand_landmarks
-
-
-class HandDetector:
-    def __init__(self):
-        self.mp_hands = mp.solutions.hands
-
-        self.hands = self.mp_hands.Hands(
-            static_image_mode=False,
-            max_num_hands=1,
-            min_detection_confidence=0.5,
-            min_tracking_confidence=0.5,
-        )
-
-    def detect(self, frame):
-        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-        results = self.hands.process(rgb)
-
-        hand_landmarks = []
-
-        if results.multi_hand_landmarks:
-            for hand in results.multi_hand_landmarks:
-                hand_landmarks.append(hand.landmark)
-
-        return DetectionResult(hand_landmarks)
+st.write("MediaPipe Version:", mp.__version__)
+st.write("MediaPipe:", dir(mp))
+st.stop()
